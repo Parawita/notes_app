414c1eb2213ec5aedda27cacb869995a8aa8862b
Parawita <kamalahmedkaml68@gmail.com>
2024-10-23 02:10:12 +0300

    inistalizw hive and open hive box

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:hive_flutter/adapters.dart';
 import 'package:notesapp/views/home_veiw.dart';
 import 'package:notesapp/views/notes_veiw.dart';
 
-void main() {
+void main() async {
+  await Hive.initFlutter();
+  await Hive.openBox('notes_box');
   runApp(const NotesApp());
 }
 
