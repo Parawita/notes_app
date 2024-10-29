9ae7dd784887d9474565990cf5ab82f65d58a4f2
Parawita <kamalahmedkaml68@gmail.com>
2024-10-29 06:32:42 +0300

    add block observer

--- lib/main.dart
+++ lib/main.dart
@@ -3,10 +3,12 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:hive_flutter/adapters.dart';
 import 'package:notesapp/Models/note_model.dart';
 import 'package:notesapp/cubtes/AddNotecubit/add.note.cubit.dart';
+import 'package:notesapp/simple_bloc_observer.dart';
 import 'package:notesapp/views/home_veiw.dart';
 import 'package:notesapp/views/notes_veiw.dart';
 
 void main() async {
+  Bloc.observer = MyBlocObserver();
   await Hive.initFlutter();
   await Hive.openBox('notes_box');
   Hive.registerAdapter(NoteModelAdapter());
---
+++ lib/simple_bloc_observer.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/widgets.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+class MyBlocObserver extends BlocObserver {
+  @override
+  void onEvent(Bloc bloc, Object? event) {
+    super.onEvent(bloc, event);
+    // TODO: implement onEvent
+  }
+
+  @override
+  void onError(BlocBase bloc, Object error, StackTrace stackTrace) {
+    debugPrint('Chang = $Change');
+  }
+
+  @override
+  void onChange(BlocBase bloc, Change change) {
+    super.onChange(bloc, change);
+    // TODO: implement onChange
+  }
+
+  @override
+  void onTransition(Bloc bloc, Transition transition) {
+    super.onTransition(bloc, transition);
+    // TODO: implement onChange
+  }
+}
